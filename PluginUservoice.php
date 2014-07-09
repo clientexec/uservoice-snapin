0a96007b1f9d43ed6c374bf41870c593c5444822
Alberto Vasquez <alberto.vasquez@gmail.com>
2014-07-09 00:14:55 -0400

    updating uservoice to match new functionality

--- PluginUservoice.php
+++ PluginUservoice.php
@@ -7,39 +7,13 @@ require_once 'modules/admin/models/SnapinPlugin.php';
 */
 class PluginUservoice extends SnapinPlugin
 {
-  var $settingsNotes;
-
-  function __construct($user, $typeOfFetch)
-  {
-      $this->settingsNotes = lang('When enabled this gives your users the ability to sign into your UserVoice account through your ClientExec\'s instance. To use it make sure your UserVoice account supports Single Sign-On (SSO) and that it\'s properly configured under the User Authentication section in your UserVoice account.');
-      parent::__construct($user, $typeOfFetch);
-  }
 
   public function getVariables() {
     $variables = array(
-      /*T*/"PublicOnly"/*/T*/ => array (
-          "type"          =>"hidden",
-          "value"         =>true
-      ),
-      'Public Name'       => array(
-          'type'        => 'hidden',
-          'description' => 'Name to be seen if viewing from public portal',
-          'value'       => 'UserVoice',
-      ),
       'Public Description'       => array(
           'type'        => 'text',
           'description' => 'Description to be seen by public',
           'value'       => 'Help us improve ClientExec by giving us your suggestions',
-      ),
-      'Public Icon'       => array(
-          'type'        => 'hidden',
-          'description' => 'Image name to use to identify snapin',
-          'value'       => 'icon-bullhorn',
-      ),
-      'Public Icon Style'       => array(
-          'type'        => 'hidden',
-          'description' => 'Custom margin based on Icon we are using',
-          'value'       => 'margin: 2px;',
       ),
       /*T*/'Plugin Name'/*/T*/       => array(
           'type'        => 'hidden',
@@ -58,23 +32,37 @@ class PluginUservoice extends SnapinPlugin
     return $variables;
   }
 
+  public function init()
+  {
+      $this->settingsNotes = lang('When enabled this gives your users the ability to sign into your UserVoice account through your ClientExec\'s instance. To use it make sure your UserVoice account supports Single Sign-On (SSO) and that it\'s properly configured under the User Authentication section in your UserVoice account.');
+      $this->addMappingForPublicMain("view", "UserVoice", 'Integrate Clientexec with Uservoice in Public Home', 'icon-bullhorn', 'margin: 2px;');
+  }
+
   public function view() {
+
+    $this->overrideTemplate = true;
+
     $returnUrl = '/';
+
     if (@$this->session->redirectUserVoice) {
       $returnUrl = $this->session->redirectUserVoice;
       unset($this->session->redirectUserVoice);
     }
+
     $url = (CE_Lib::isHttps()? 'https://' : 'http://')
       .$this->settings->get('plugin_uservoice_Subdomain')
       . ".uservoice.com$returnUrl";
+
     if (!$this->user->isAnonymous()) {
       // don't wanna have an "Anonymous"-labeled user in uservoice
       $url .= '?sso=' . $this->getUrl($this->user);
     }
+
     return "<script>location.href = '$url';</script>";
   }
 
   private function getUrl(User $user) {
+
     $uservoice_subdomain = $this->settings->get('plugin_uservoice_Subdomain');
     $sso_key = $this->settings->get('plugin_uservoice_SSO key');
 
@@ -104,7 +92,7 @@ class PluginUservoice extends SnapinPlugin
 
     $pad = 16 - (strlen($data) % 16);
     $data = $data . str_repeat(chr($pad), $pad);
-      
+
     $cipher = mcrypt_module_open(MCRYPT_RIJNDAEL_128,'','cbc','');
     mcrypt_generic_init($cipher, $saltedHash, $iv);
     $encryptedData = mcrypt_generic($cipher,$data);
